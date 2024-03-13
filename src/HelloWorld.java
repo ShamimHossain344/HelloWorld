553a5cd13ac8a1de96e3cf0ceff77a11d17ef492
SHAMIM_HOSSAIN <shamimkhondokar0@gmail.com>
2024-03-14 00:22:45 +0600

    Two constructor and setter getter methods added.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,57 @@
 public class HelloWorld {
+    public int roll;
+    public String name;
+    public boolean passed;
+    public HelloWorld(){//Constructor
+        roll = 18;
+        name = "Shamim";
+        passed = true;
+    }
+    public HelloWorld(int roll , String name, boolean passed){//Constructor
+        this.roll = roll;
+        this.name = name;
+        this.passed = passed;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public boolean isPassed() {
+        return passed;
+    }
+
+    // joto gulo Globally variable thakbe totogulo getter setter method hobe
+    //  getter method
+   public String getName(){
+        return name;
+    }
+   /* public void setName(String n){//  n local name
+        name = n; //  name global name
+    }*/
+
+    public void setPassed(boolean passed) {
+        this.passed = passed;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    // setter method
+   public void setName(String name){//  name local name
+       this.name = name; //  this. means current class
+   }
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        HelloWorld helloWorld = new HelloWorld();
+        HelloWorld helloWorld2 = new HelloWorld(71,"SHU",true);
+        helloWorld.setName("Hossain");
+        helloWorld.setRoll(17);
+        helloWorld.setPassed(false);
+        System.out.println(helloWorld2.getName());
+        System.out.println(helloWorld.getRoll());
+        System.out.println(helloWorld2.isPassed());
     }
 }
+
+
